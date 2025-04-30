f35593f7809f0067178c198cf04017450b842c39
Jan Zimola <zimolajan789@gmail.com>
2025-04-30 07:43:16 +0200

    queue sync on resumed

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,21 +1,47 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:custom_supabase_drift_sync/presentation/module/module.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class App extends HookConsumerWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+    // Initialize providers
+    ref.read(authStateProvider);
+    ref.read(syncMangerPProvider);
+    //ref.read(supabaseConnectorPProvider);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    //TODO: Needs testing
+    if (state == AppLifecycleState.resumed) {
+      ref.read(syncMangerPProvider).queueSync();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final router = ref.watch(appRouterProvider);
-    useEffect(() {
-      ref.read(authStateProvider);
-      ref.read(syncMangerPProvider);
-      //ref.read(supabaseConnectorPProvider);
-      return null;
-    }, []);
+
     return AdaptiveTheme(
         light: ThemeData.light(useMaterial3: true),
         dark: ThemeData.dark(useMaterial3: true),
